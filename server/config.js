bec84c88579e5a753b13aa0494419cb9e21bb8e3
simplerogic <lajohnson12@gmail.com>
2018-05-26 23:28:02 -0600

    changed it for prod too

--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
 const config = {
   mongoURL: process.env.MONGO_URL || 'mongodb://db:27017/todo-v2',
-  port: process.env.PORT || 80,
+  port: process.env.PORT || 8000,
   secret: process.env.SECRET || 'T@5kM4nag3R',
 };
 
